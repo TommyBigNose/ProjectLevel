d8c072f338833a6578ce2dfa49d119a56a808212
ZERO-COOL\thoma <thomaslachin89@gmail.com>
2022-02-13 12:25:09 -0600

    Updated test loot for all military types

--- Source/ProjectLevel/ProjectLevel.Tests/TestHelpers/TestDataSource.cs
+++ Source/ProjectLevel/ProjectLevel.Tests/TestHelpers/TestDataSource.cs
@@ -27,7 +27,7 @@ namespace ProjectLevel.Tests.TestHelpers
 				},
 				new Loot()
 				{
-					Name = "Crap sword",
+					Name = "Melee - Speed up",
 					Level = 1,
 					GoldValue = 5,
 
@@ -39,33 +39,14 @@ namespace ProjectLevel.Tests.TestHelpers
 						{
 							MilitaryType = MilitaryType.Melee,
 							DamageRatio = 0.1f,
-							SpeedRatio = 0.1f,
+							SpeedRatio = 100.0f,
 							RecruitRatio = 0.0f
 						}
 					}
 				},
 				new Loot()
 				{
-					Name = "Crap sword + 1",
-					Level = 1,
-					GoldValue = 5,
-
-					GoldIncome = 0,
-					GoldSpeedRatio = 0.0f,
-					MilitaryLootStats = new List<MilitaryLootStat>
-					{
-						new MilitaryLootStat()
-						{
-							MilitaryType = MilitaryType.Melee,
-							DamageRatio = 0.1f,
-							SpeedRatio = 0.1f,
-							RecruitRatio = 0.0f
-						}
-					}
-				},
-				new Loot()
-				{
-					Name = "Crap bow",
+					Name = "Ranged - Speed up",
 					Level = 1,
 					GoldValue = 5,
 
@@ -77,7 +58,26 @@ namespace ProjectLevel.Tests.TestHelpers
 						{
 							MilitaryType = MilitaryType.Ranged,
 							DamageRatio = 0.1f,
-							SpeedRatio = 0.1f,
+							SpeedRatio = 100.0f,
+							RecruitRatio = 0.0f
+						}
+					}
+				},
+				new Loot()
+				{
+					Name = "Siege - Speed up",
+					Level = 1,
+					GoldValue = 5,
+
+					GoldIncome = 0,
+					GoldSpeedRatio = 0.0f,
+					MilitaryLootStats = new List<MilitaryLootStat>
+					{
+						new MilitaryLootStat()
+						{
+							MilitaryType = MilitaryType.Siege,
+							DamageRatio = 0.1f,
+							SpeedRatio = 100.0f,
 							RecruitRatio = 0.0f
 						}
 					}
